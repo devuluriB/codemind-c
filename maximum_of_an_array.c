d873682a06b3ca3cf40de579d66a62270814aab2
DEVULURI BALASATYANARAYANA <97426245+devuluriB@users.noreply.github.com>
2022-07-03 18:45:09 +0530

    #include<stdio.h> int main() {     int n,i,a[100],mi;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     mi=a[0];     for(i=0;i<n;i++)     {         if(a[i]>mi)         {             mi=a[i];         }     }     printf("%d",mi); }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],mi;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    mi=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>mi)
+        {
+            mi=a[i];
+        }
+    }
+    printf("%d",mi);
+}
