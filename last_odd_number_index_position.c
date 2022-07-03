59ef1d817c330055978cdc06be0073bb76508239
DEVULURI BALASATYANARAYANA <97426245+devuluriB@users.noreply.github.com>
2022-07-03 18:49:38 +0530

    #include<stdio.h> int main() {     int n,i,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=n-1;i>=0;i--)     {         if(a[i]%2!=0)         {             printf("%d",i);             return 0;         }     } }

---
+++ last_odd_number_index_position.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]%2!=0)
+        {
+            printf("%d",i);
+            return 0;
+        }
+    }
+}
